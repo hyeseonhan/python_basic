8a1e87134065883e057889da7ef5ce7b186c4a8a
HyeSeon Han <68494080+hyeseonhan@users.noreply.github.com>
2022-05-17 11:26:21 +0000

    Day 1 of 14

---
+++ main.py
@@ -0,0 +1,53 @@
+a_string = "like this"
+a_number = 3
+a_boolean = False
+a_float = 3.12
+a_none = None
+
+print(type(a_none))
+print(type(a_float))
+
+
+days = ["Mon" , "Tue", "Wed", "Thur", "Fri"]
+print(days)
+print(type(days))
+
+print("Mon" in days)
+print("Man" in days)
+print(days[3])
+print(len(days[3]))
+
+print(days)
+days.append("Sat")
+days.reverse()
+print(days)
+
+
+days_1 = ("Mon" , "Tue", "Wed", "Thur", "Fri")
+print(type(days_1))
+
+
+
+nico = {
+"name" : "Nico",
+"age" : 29,
+"korean" : True,
+"fav_food": ["Kinchi", "Sashimi"]
+}
+
+print(nico["fav_food"])
+nico["handsome"] = True
+print(nico) 
+
+
+print(len("ddfasdfsdafsdafdfaf"))
+
+age = "18"
+print(age)
+print(type(age))
+print("-------------------------")
+n_age = int(age)
+print(n_age)
+print(type(n_age))
+
+print("--------------------Day 2 of 14-------------------")
